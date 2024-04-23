25858be5ad78a5a687de4d391657b18bb86c1c59
Layla Abkari <lolabkari@gmail.com>
2024-04-24 00:38:54 +0100

    Markdown to HTML;

--- markdown2html.py
+++ markdown2html.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 import sys
 import os
-import markdown
 
 
-def markdown_to_html(input_f, output_f):
+def markdown_to_html(markdown_f, output_f):
     """Convert Markdown file to HTML file."""
-    with open(input_f, "r") as f:
+    if not os.path.exists(markdown_f):
+        print(f"Missing {markdown_f}", file=sys.stderr)
+        sys.exit(1)
+    with open(markdown_f, "r") as f:
         md_text =  f.read()
-    html_text = markdown.markdown(md_text)
     with open(output_f, 'w') as f:
-        f.write(html_text)
-    print("HTML file successfully created:", output_f)
+        f.write(md_text)
 
 def main():
     """Main function to parse command line args and convert Markdown to HTML"""
-    if len(sys.argv) != 3:
-        sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
         sys.exit(1)
-        input_f = sys.argv[1]
-        output_f = sys.argv[2]
-        if not os.path.exists(input_f):
-            sys.stderr.write(f"Missing {input_f}\n")
-            sys.exit(1)
 
-        markdown_to_html(input_f, output_f)
-        sys.exit(0)
+    markdown_f = sys.argv[1]
+    output_f = sys.argv[2]
     
+    markdown_to_html(markdown_f, output_f)
+    sys.exit(0)    
+
 if __name__ == "__main__":
     main()
