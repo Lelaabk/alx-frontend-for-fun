d7e1fb1e73792db3a2edc2cae3e55ba5fd0dafdc
Layla Abkari <lolabkari@gmail.com>
2024-04-24 00:01:38 +0100

    Markdown to HTML;

--- markdown2html.py
+++ markdown2html.py
@@ -11,3 +11,16 @@ def markdown_to_html(input_f, output_f):
     html_text = markdown.markdown(md_text)
     with open(output_f, 'w') as f:
         f.write(html_text)
+
+def main():
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
+        sys.exit(1)
+        input_f = sys.argv[1]
+        output_f = sys.argv[2]
+        if not os.path.exists(input_f):
+            sys.stderr.write(f"Missing {input_f}\n")
+            sys.exit(1)
+
+        markdown_to_html(input_f, output_f)
+        sys.exit(0)
