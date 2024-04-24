2f9114a91fcd5f98af612c8984393829f2d241b6
Layla Abkari <lolabkari@gmail.com>
2024-04-24 01:06:07 +0100

    Markdown to HTML;

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
 import os
+import re
+import hashlib
 
 
 def markdown_to_html(markdown_f, output_f):
@@ -8,11 +10,21 @@ def markdown_to_html(markdown_f, output_f):
     if not os.path.exists(markdown_f):
         print(f"Missing {markdown_f}", file=sys.stderr)
         sys.exit(1)
-    with open(markdown_f, "r") as f:
-        md_text = f.read()
-    with open(output_f, 'w') as f:
-        f.write(md_text)
 
+    with open(markdown_f, "r") as f:
+        lines = f.readlines()
+
+    html_lines = []
+    for line in lines:
+        line = line.strip()
+        line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
+        line = re.sub(r'__(.*?)__', r'<em>\1</em>', line)
+        line = re.sub(r'\[\[(.*?)\]\]', lambda x: hashlib.md5(x.group(1).encode()).hexdigest(), line)
+        line = re.sub(r'\(\((.*?)\)\)', lambda x: x.group(1).replace('c', '').replace('C', ''), line)
+        html_lines.append(line)
+
+    with open(output_f, 'w') as f:
+        f.write('\n'.join(html_lines))
 
 def main():
     """Main function to parse command line args and convert Markdown to HTML"""
